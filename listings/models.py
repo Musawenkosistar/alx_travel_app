72cdc7573e0dee42a650316d4178fbb3a3282617
Musawenkosistar <musawenkosi2504@gmail.com>
2025-11-16 18:12:27 +0000

    Fix: remove lock

--- listings/models.py
+++ listings/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Listing(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
-    location = models.CharField(max_length=255)
+    location = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
